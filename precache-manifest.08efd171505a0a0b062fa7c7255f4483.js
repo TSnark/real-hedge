9bb7df4b923eba60e4278899b1c7a4f8c8f05cbd
TSnark <753334917@protonmail.com>
2020-09-27 18:01:33 +0000

    gh-pages

---
+++ precache-manifest.08efd171505a0a0b062fa7c7255f4483.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a45a4138cda4e90e45df517fcba0f24",
+    "url": "/real-hedge/index.html"
+  },
+  {
+    "revision": "71555d4377b512bb21bd",
+    "url": "/real-hedge/static/css/main.75cf32e2.chunk.css"
+  },
+  {
+    "revision": "07d969b3e006bb9ef995",
+    "url": "/real-hedge/static/js/0.11d38f6e.chunk.js"
+  },
+  {
+    "revision": "3556bc48237e92a79a0a",
+    "url": "/real-hedge/static/js/10.ab5faea1.chunk.js"
+  },
+  {
+    "revision": "b53d3de55a01fb410616",
+    "url": "/real-hedge/static/js/3.b9ca602a.chunk.js"
+  },
+  {
+    "revision": "d82d16426d081ec94136ba26dece66d9",
+    "url": "/real-hedge/static/js/3.b9ca602a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f064cd8e3adfcf6624",
+    "url": "/real-hedge/static/js/4.3924b3fb.chunk.js"
+  },
+  {
+    "revision": "dd5f9dae71043eea6b57",
+    "url": "/real-hedge/static/js/5.4a0a21b6.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/real-hedge/static/js/5.4a0a21b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5af7c47559a1e873474",
+    "url": "/real-hedge/static/js/6.d789032f.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/real-hedge/static/js/6.d789032f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07177f40d2c1afff254b",
+    "url": "/real-hedge/static/js/7.0d4f1e49.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/real-hedge/static/js/7.0d4f1e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f714e15b5d1eda94753",
+    "url": "/real-hedge/static/js/8.e27da616.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/real-hedge/static/js/8.e27da616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e97a4300df80c516195",
+    "url": "/real-hedge/static/js/9.f020a085.chunk.js"
+  },
+  {
+    "revision": "71555d4377b512bb21bd",
+    "url": "/real-hedge/static/js/main.2f36c470.chunk.js"
+  },
+  {
+    "revision": "9b48b48fec24e22744f0",
+    "url": "/real-hedge/static/js/runtime-main.1ec048ea.js"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/real-hedge/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/real-hedge/static/media/wallet-connect.37f2bc6e.svg"
+  }
+]);
